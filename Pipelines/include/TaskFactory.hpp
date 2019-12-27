a78c0f888d7f9136af8afa17a198d4cbd7964cc6
Your Name <you@example.com>
2019-12-27 23:24:18 +0100

    Compile time check of TaskFactory::wait_all(...). Does not currently work

--- Pipelines/include/TaskFactory.hpp
+++ Pipelines/include/TaskFactory.hpp
@@ -2,16 +2,37 @@
 #define TASKFACTORY_HPP
 #include <functional>
 #include <future>
-#include <algorithm>
-
-
-
-/*
-* TODO:
-* - Simplify interface
-*/
+#include <type_traits>
 
 namespace PLS {
+
+  // inspiration from https://stackoverflow.com/questions/29603364/type-trait-to-check-that-all-types-in-a-parameter-pack-are-copy-constructible/29603857#29603857
+  template<typename... Conds>
+  struct and_ : std::true_type {};
+
+  template<typename Cond, typename... Conds>
+  struct and_<Cond, Conds...>
+  : std::conditional_t<Cond::value, and_<Conds...>, std::false_type>
+  { };
+
+  // SFINAE
+  // https://gist.github.com/fenbf/d2cd670704b82e2ce7fd
+  template<typename T>
+  class has_wait {
+    typedef char Yes[1];
+    typedef char No[2];
+
+    template<typename C> static Yes& test( decltype(&C::wait) );
+    template<typename C> static No& test(...);
+  public:
+    static const bool value = sizeof(test<T>(0)) == sizeof(Yes);
+    //enum { value = sizeof(test<T>(0)) == sizeof(Yes) };
+  };
+
+  template<typename... Args>
+  using is_awaitable = std::conjunction<has_wait<Args>...>;
+
+
   class TaskFactory
   {
   private:
@@ -19,12 +40,27 @@ namespace PLS {
   public:
     TaskFactory();
     ~TaskFactory();
-    template<class FuncT>
-    std::packaged_task<FuncT> startAsyncTask(std::function<FuncT>&& callable)
+
+    template<class...Args>
+    void wait_all(Args&& ...args)
     {
-      std::packaged_task<FuncT> task(callable);
-      return task;
+      static_assert(is_awaitable<Args...>::value, "Argument in TaskFactory::wait_all(...) does not implement the wait function");
     }
+    
+
+    template<class F, class... Args, typename R = std::result_of_t<std::decay_t<F>&&(Args...)>>
+    std::future<R> startAsyncTask(F&& callable, Args&&... args)
+    {
+      std::future<R> task = std::async(std::launch::async, callable, std::ref(args)...); 
+      return task;
+    } 
+
+    // template<class FuncT>
+    // std::packaged_task<FuncT> startAsyncTask(std::function<FuncT>&& callable)
+    // {
+    //   std::packaged_task<FuncT> task(callable);
+    //   return task;
+    // }
 
     // https://stackoverflow.com/questions/33135304/c-create-an-stdpackaged-task-of-a-generic-stdbind
     // template<class F, class...Args, typename R = std::result_of_t<std::decay_t<F>&&(Args...)>>
@@ -38,30 +74,14 @@ namespace PLS {
     // }
 
     // We create and execute the task imidiatly, thus only returning a future
-    template<class F, class...Args, typename R = std::result_of_t<std::decay_t<F>&&(Args...)>>
-    std::future<R> startAsyncTaskThreaded(F&& callable, Args&&...args)
-    {
-      std::packaged_task<R(Args...)> task(callable);
-      std::thread task_td(std::move(task), args...);
-      task_td.detach();
-      return task.get_future();
-    }
-
-    template<class F, class... Args, typename R = std::result_of_t<std::decay_t<F>&&(Args...)>>
-    std::future<R> startAsyncTask(F&& callable, Args&&... args)
-    {
-      std::future<R> task = std::async(std::launch::async, callable, std::ref(args)...); 
-      
-
-
-
-      // std::packaged_task<R(Args...)> task(callable);
-      // std::thread task_td(std::move(task), args...);
-      // task_td.detach();
-      return task;
-    } 
-
-    
+    // template<class F, class...Args, typename R = std::result_of_t<std::decay_t<F>&&(Args...)>>
+    // std::future<R> startAsyncTaskThreaded(F&& callable, Args&&...args)
+    // {
+    //   std::packaged_task<R(Args...)> task(callable);
+    //   std::thread task_td(std::move(task), args...);
+    //   task_td.detach();
+    //   return task.get_future();
+    // }
   };  
 }
 
