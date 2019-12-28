997b56adba0922dc47cf770f4a755c55c6d0a2bf
Your Name <you@example.com>
2019-12-28 16:58:22 +0100

    Implemented wait_all functionality

--- Pipelines/include/TaskFactory.hpp
+++ Pipelines/include/TaskFactory.hpp
@@ -25,11 +25,20 @@ namespace PLS {
     TaskFactory();
     ~TaskFactory();
 
-    template<class...Args>
-    void wait_all(Args&& ...args)
+    template<typename T, typename...Args>
+    void wait_all(T&& first, Args&& ...args)
     {
       static_assert(is_awaitable<Args...>::value, "Argument in TaskFactory::wait_all(...) does not implement the wait function");
+      first.wait();
+      wait_all(args...);
     }
+
+    template<typename T>
+    void wait_all(T&& future)
+    {
+      future.wait();
+    }
+
     
 
     template<class F, class... Args, typename R = std::result_of_t<std::decay_t<F>&&(Args...)>>
@@ -37,35 +46,7 @@ namespace PLS {
     {
       std::future<R> task = std::async(std::launch::async, callable, std::ref(args)...); 
       return task;
-    } 
-
-    // template<class FuncT>
-    // std::packaged_task<FuncT> startAsyncTask(std::function<FuncT>&& callable)
-    // {
-    //   std::packaged_task<FuncT> task(callable);
-    //   return task;
-    // }
-
-    // https://stackoverflow.com/questions/33135304/c-create-an-stdpackaged-task-of-a-generic-stdbind
-    // template<class F, class...Args, typename R = std::result_of_t<std::decay_t<F>&&(Args...)>>
-    // std::packaged_task<R(Args...)> startAsyncTask(F&& callable, Args&&...args)
-    // {
-    //   std::packaged_task<R(Args...)> task(
-    //     [callable = std::forward<F>(callable)] (auto&&...args) {
-    //       callable(args...);
-    //   });
-    //   return task;
-    // }
-
-    // We create and execute the task imidiatly, thus only returning a future
-    // template<class F, class...Args, typename R = std::result_of_t<std::decay_t<F>&&(Args...)>>
-    // std::future<R> startAsyncTaskThreaded(F&& callable, Args&&...args)
-    // {
-    //   std::packaged_task<R(Args...)> task(callable);
-    //   std::thread task_td(std::move(task), args...);
-    //   task_td.detach();
-    //   return task.get_future();
-    // }
+    }
   };  
 }
 
