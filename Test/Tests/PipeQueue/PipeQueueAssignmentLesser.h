d0b6ddb928ede5e340d01b947cbe89a9c9d1670f
Your Name <you@example.com>
2019-12-29 20:23:25 +0100

    Test added for pipeQueue: '==', '!=', '<', '<<', '>>', and 'swap'

--- Test/Tests/PipeQueue/PipeQueueAssignmentLesser.h
+++ Test/Tests/PipeQueue/PipeQueueAssignmentLesser.h
@@ -1,1 +1,62 @@
 #include "../ClassHeader.h"
+
+
+TEST(PipeQueueTestAssignmentLesser, lesser_true_lesser)
+{
+    PLS::PipeQueue<int> pipeA = {1, 2};
+    PLS::PipeQueue<int> pipeB = {1, 2, 3};
+
+    ASSERT_NO_THROW(pipeA < pipeB);
+    ASSERT_TRUE(pipeA < pipeB);
+}
+
+
+TEST(PipeQueueTestAssignmentLesser, lesser_true_lesser_one_empty)
+{
+    PLS::PipeQueue<int> pipeA;;
+    PLS::PipeQueue<int> pipeB = {1, 2, 3};
+
+    ASSERT_NO_THROW(pipeA < pipeB);
+    ASSERT_TRUE(pipeA < pipeB);
+}
+
+
+TEST(PipeQueueTestAssignmentLesser, lesser_false_larger)
+{
+    PLS::PipeQueue<int> pipeA = {1, 2, 3, 4};
+    PLS::PipeQueue<int> pipeB = {1, 2, 3};
+
+    ASSERT_NO_THROW(pipeA < pipeB);
+    ASSERT_FALSE(pipeA < pipeB);
+}
+
+
+TEST(PipeQueueTestAssignmentLesser, lesser_false_larger_one_empty)
+{
+    PLS::PipeQueue<int> pipeA = {1, 2, 3, 4};
+    PLS::PipeQueue<int> pipeB;
+
+    ASSERT_NO_THROW(pipeA < pipeB);
+    ASSERT_FALSE(pipeA < pipeB);
+}
+
+
+TEST(PipeQueueTestAssignmentLesser, lesser_false_equal_empty)
+{
+    PLS::PipeQueue<int> pipeA;
+    PLS::PipeQueue<int> pipeB;
+
+    ASSERT_NO_THROW(pipeA < pipeB);
+    ASSERT_FALSE(pipeA < pipeB);
+}
+
+
+TEST(PipeQueueTestAssignmentLesser, lesser_false_equal_filled)
+{
+    PLS::PipeQueue<int> pipeA = {1, 2, 3};
+    PLS::PipeQueue<int> pipeB = {1, 2, 3};
+
+    ASSERT_NO_THROW(pipeA < pipeB);
+    ASSERT_FALSE(pipeA < pipeB);
+}
+
--- Test/Tests/PipeQueue/PipeQueueSwap.h
+++ Test/Tests/PipeQueue/PipeQueueSwap.h
@@ -1,1 +1,85 @@
 #include "../ClassHeader.h"
+
+TEST(PipeQueueTestSwap, swap_empty)
+{
+    PLS::PipeQueue<int> pipeA;
+    PLS::PipeQueue<int> pipeACopy(pipeA);
+    PLS::PipeQueue<int> pipeB;
+    PLS::PipeQueue<int> pipeBCopy(pipeB);
+
+    ASSERT_NO_THROW(pipeA.swap(pipeB));
+
+    ASSERT_TRUE(pipeA == pipeBCopy);
+    ASSERT_TRUE(pipeB == pipeACopy);
+
+}
+
+
+TEST(PipeQueueTestSwap, swap_filled_same_size)
+{
+    PLS::PipeQueue<int> pipeA = {1, 2, 3};
+    PLS::PipeQueue<int> pipeACopy(pipeA);
+    PLS::PipeQueue<int> pipeB = {4, 5, 6};
+    PLS::PipeQueue<int> pipeBCopy(pipeB);
+
+    ASSERT_NO_THROW(pipeA.swap(pipeB));
+
+    ASSERT_TRUE(pipeA == pipeBCopy);
+    ASSERT_TRUE(pipeB == pipeACopy);
+}
+
+
+TEST(PipeQueueTestSwap, swap_filled_diff_size_to)
+{
+    PLS::PipeQueue<int> pipeA = {1, 2, 3};
+    PLS::PipeQueue<int> pipeACopy(pipeA);
+    PLS::PipeQueue<int> pipeB = {1, 2, 3, 4, 5, 6};
+    PLS::PipeQueue<int> pipeBCopy(pipeB);
+
+    ASSERT_NO_THROW(pipeA.swap(pipeB));
+
+    ASSERT_TRUE(pipeA == pipeBCopy);
+    ASSERT_TRUE(pipeB == pipeACopy);
+}
+
+
+TEST(PipeQueueTestSwap, swap_filled_diff_size_from)
+{
+    PLS::PipeQueue<int> pipeA = {1, 2, 3, 4, 5, 6};
+    PLS::PipeQueue<int> pipeACopy(pipeA);
+    PLS::PipeQueue<int> pipeB = {1, 2, 3};
+    PLS::PipeQueue<int> pipeBCopy(pipeB);
+
+    ASSERT_NO_THROW(pipeA.swap(pipeB));
+
+    ASSERT_TRUE(pipeA == pipeBCopy);
+    ASSERT_TRUE(pipeB == pipeACopy);
+}
+
+
+TEST(PipeQueueTestSwap, swap_empty_and_filled)
+{
+    PLS::PipeQueue<int> pipeA;
+    PLS::PipeQueue<int> pipeACopy(pipeA);
+    PLS::PipeQueue<int> pipeB = {1, 2, 3, 4};
+    PLS::PipeQueue<int> pipeBCopy(pipeB);
+
+    ASSERT_NO_THROW(pipeA.swap(pipeB));
+
+    ASSERT_TRUE(pipeA == pipeBCopy);
+    ASSERT_TRUE(pipeB == pipeACopy);
+}
+
+
+TEST(PipeQueueTestSwap, swap_filled_and_empty)
+{
+    PLS::PipeQueue<int> pipeA = {1, 2, 3, 4};
+    PLS::PipeQueue<int> pipeACopy(pipeA);
+    PLS::PipeQueue<int> pipeB;
+    PLS::PipeQueue<int> pipeBCopy(pipeB);
+
+    ASSERT_NO_THROW(pipeA.swap(pipeB));
+
+    ASSERT_TRUE(pipeA == pipeBCopy);
+    ASSERT_TRUE(pipeB == pipeACopy);
+}
