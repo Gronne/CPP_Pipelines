a25ee95d959b0ca0f622636061b83bebb21dc97b
Your Name <you@example.com>
2019-12-29 22:17:43 +0100

    Install environment bash file have been added

---
+++ installEnvironment.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Install developer environment
+
+SOURCEDIR=$(pwd)
+
+sudo apt-get install libgtest-dev
+sudo apt-get install cmake
+
+cd ~
+cd /usr/src/gtest
+
+sudo cmake CMakeLists.txt
+sudo make
+
+sudo cp *.a /usr/lib 
+
+cd ~
+cd $SOURCEDIR
+
+cmake CMakeLists.txt 
+make 
+
+cd Test
+cmake CMakeLists.txt
+make
